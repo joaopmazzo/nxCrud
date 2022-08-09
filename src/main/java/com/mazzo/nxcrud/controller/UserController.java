980890155e3d6408c6b267f57cf1d75e44902cbc
João Paulo Mazzo <mazzojp@gmail.com>
2022-08-09 18:34:36 -0300

    Create UserController.java

--- src/main/java/com/mazzo/nxcrud/controller/UserController.java
+++ src/main/java/com/mazzo/nxcrud/controller/UserController.java
@@ -1,2 +1,15 @@
-package com.mazzo.nxcrud.controller;public class UserController {
+package com.mazzo.nxcrud.controller;
+
+import com.mazzo.nxcrud.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/user")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class UserController {
+
+
 }
