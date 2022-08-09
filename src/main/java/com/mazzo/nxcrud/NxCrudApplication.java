979976a915b4c6da214a03d215816366169e9663
João Paulo Mazzo <mazzojp@gmail.com>
2022-08-09 18:33:19 -0300

    First commit

--- src/main/java/com/mazzo/nxcrud/NxCrudApplication.java
+++ src/main/java/com/mazzo/nxcrud/NxCrudApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NxCrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NxCrudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NxCrudApplication.class, args);
+    }
 
 }
