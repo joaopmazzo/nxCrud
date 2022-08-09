99b812bd01df4af09fb92cd68b84a5decac79cbb
João Paulo Mazzo <mazzojp@gmail.com>
2022-08-09 18:34:59 -0300

    Create UserMapper.java

--- src/main/java/com/mazzo/nxcrud/mapper/UserMapper.java
+++ src/main/java/com/mazzo/nxcrud/mapper/UserMapper.java
@@ -1,2 +1,17 @@
-package com.mazzo.nxcrud.mapper;public class UserMapper {
+package com.mazzo.nxcrud.mapper;
+
+import com.mazzo.nxcrud.dto.request.UserDTO;
+import com.mazzo.nxcrud.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    User toModel(UserDTO userDTO);
+    UserDTO toDTO(User user);
+
 }
