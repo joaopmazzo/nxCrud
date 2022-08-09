6e5f67c51da054d5dd7519580c47a844282f975b
João Paulo Mazzo <mazzojp@gmail.com>
2022-08-09 18:34:46 -0300

    Create UserDTO.java

--- src/main/java/com/mazzo/nxcrud/dto/request/UserDTO.java
+++ src/main/java/com/mazzo/nxcrud/dto/request/UserDTO.java
@@ -1,2 +1,20 @@
-package com.mazzo.nxcrud.dto.request;public class UserDTO {
+package com.mazzo.nxcrud.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private String id;
+
+    @NotNull
+    private String name;
+
 }
