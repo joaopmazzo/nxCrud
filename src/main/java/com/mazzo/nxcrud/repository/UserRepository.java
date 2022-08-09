1686159a093bf974bb6d961ff572412a6c9df278
João Paulo Mazzo <mazzojp@gmail.com>
2022-08-09 18:35:07 -0300

    Create UserRepository.java

--- src/main/java/com/mazzo/nxcrud/repository/UserRepository.java
+++ src/main/java/com/mazzo/nxcrud/repository/UserRepository.java
@@ -1,2 +1,8 @@
-package com.mazzo.nxcrud.repository;public class UserRepository {
+package com.mazzo.nxcrud.repository;
+
+import com.mazzo.nxcrud.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
 }
