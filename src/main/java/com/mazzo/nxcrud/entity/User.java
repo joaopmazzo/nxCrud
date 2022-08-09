4329346a97dc388aa8da037bba29fcf70df05c96
João Paulo Mazzo <mazzojp@gmail.com>
2022-08-09 18:34:11 -0300

    Create User.java

--- src/main/java/com/mazzo/nxcrud/entity/User.java
+++ src/main/java/com/mazzo/nxcrud/entity/User.java
@@ -1,2 +1,23 @@
-package com.mazzo.nxcrud.entity;public class User {
+package com.mazzo.nxcrud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String document;
+    private String email;
+    private String city;
+    private String state;
+
 }
