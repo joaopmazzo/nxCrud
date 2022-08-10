953306b2d7e8d26b0bac615301ecf19d00c2d739
João Paulo Mazzo <mazzojp@gmail.com>
2022-08-09 22:46:18 -0300

    Create new exception to handle with user not found error

---
+++ src/main/java/com/mazzo/nxcrud/exception/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package com.mazzo.nxcrud.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends Exception {
+
+    public UserNotFoundException(String id) {
+        super("User not found with ID "+id);
+    }
+
+}
