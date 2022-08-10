733c4d8753b04427e948cdd95ebb18c527d787b2
João Paulo Mazzo <mazzojp@gmail.com>
2022-08-09 22:45:28 -0300

    Refactor imports

--- src/main/java/com/mazzo/nxcrud/mapper/UserMapper.java
+++ src/main/java/com/mazzo/nxcrud/mapper/UserMapper.java
@@ -3,7 +3,6 @@ package com.mazzo.nxcrud.mapper;
 import com.mazzo.nxcrud.dto.request.UserDTO;
 import com.mazzo.nxcrud.entity.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
