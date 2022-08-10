70cf89f1c1c1773ad0f5c5cc6922cb441ac0fe5a
João Paulo Mazzo <mazzojp@gmail.com>
2022-08-09 22:50:32 -0300

    Add @builder annotation to the entity

--- src/main/java/com/mazzo/nxcrud/entity/User.java
+++ src/main/java/com/mazzo/nxcrud/entity/User.java
@@ -1,12 +1,15 @@
 package com.mazzo.nxcrud.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "users")
