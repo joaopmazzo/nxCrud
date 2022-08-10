3d53f4df99c5a16004c85ad295e1c7621657c3ba
João Paulo Mazzo <mazzojp@gmail.com>
2022-08-09 22:45:02 -0300

    Add new params to validate methods

--- src/main/java/com/mazzo/nxcrud/dto/request/UserDTO.java
+++ src/main/java/com/mazzo/nxcrud/dto/request/UserDTO.java
@@ -1,6 +1,8 @@
 package com.mazzo.nxcrud.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +17,23 @@ public class UserDTO {
     private String id;
 
     @NotNull
+    @Size(min = 2, max = 100)
     private String name;
+
+    @NotNull
+    @Size(min = 11, max = 16)
+    private String document;
+
+    @NotNull
+    @Email
+    private String email;
+
+    @NotNull
+    @Size(min = 2, max = 100)
+    private String city;
+
+    @NotNull
+    @Size(min = 2, max = 100)
+    private String state;
 
 }
